8a4d3a237c1c36bbf10ab5f50413777306bb8b16
vincnetas <vincnetas@gmail.com>
2015-03-11 21:18:12 +0200

    env dns

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function (req, res, next) {
     var collection = req.db.get("users");
     collection.find({}, {}, function (err, docs) {
         res.render('index', {
-            title: 'Express',
+            title: 'Express ' + process.env.OPENSHIFT_GEAR_DNS,
             length: docs.length
         });
     });
